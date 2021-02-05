6bfcd92126d41a645a6fcb519afb4296bd85352e
Taylor Lea <taylorl@jfrog.com>
2021-02-04 16:08:18 -0800

    try to upload filespec file to vm

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -1,6 +1,6 @@
 deployApplication() {
-  printenv |  grep -i myApp
-  scp -i ~/.ssh/myVM "$res_myApp_path" 192.168.50.19:/opt/
+  ls $res_myApp_resourcePath
+#  scp -i ~/.ssh/myVM "$res_myApp_path" 192.168.50.19:/opt/
 #  local success=true
 #  local url=$(find_step_configuration_value "healthCheckUrl")
 #  {
