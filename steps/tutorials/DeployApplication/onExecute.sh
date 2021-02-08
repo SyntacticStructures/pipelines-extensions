381ea7aa8f9ee1063c88e5391c975fb27b9ccfdb
Taylor Lea <taylorl@jfrog.com>
2021-02-08 12:52:46 -0800

    printing env

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -1,5 +1,5 @@
 deployApplication() {
-  printenv | grep -i sshKey
+  printenv
   scp -i ~/.ssh/myVM "$res_myApp_resourcePath" 192.168.50.19:/opt/
 #  local success=true
 #  local url=$(find_step_configuration_value "healthCheckUrl")
@@ -13,7 +13,6 @@ deployApplication() {
 #    echo "Health check succeeded"
 #  fi
 
-#  $success
   echo "deployApplication running"
 }
 
