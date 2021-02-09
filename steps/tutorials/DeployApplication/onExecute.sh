911af13e473245836d3532855685ee3e6e5b7eef
Taylor Lea <taylorl@jfrog.com>
2021-02-09 13:48:01 -0800

    operation

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -5,8 +5,8 @@ deployApplication() {
 
 #  local ip_addr=$(jq "$res_myVM_targets"[0] --raw-output --null-input)
 #  local ssh_id="$HOME/.ssh/myVM"
-  get_resource_name --type VmCluster
-  get_resource_name --type FileSpec
+  get_resource_name --type VmCluster --operation IN
+  get_resource_name --type FileSpec --operation IN
 #  rsync -e "ssh -i $ssh_id" "$res_myApp_resourcePath" "$ip_addr":"$step_configuration_targetDirectory"
 #  ssh -i "$ssh_id" "$ip_addr" "cd $step_configuration_targetDirectory; $step_configuration_deployCommand"
 
