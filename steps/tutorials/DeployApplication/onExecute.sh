cf0abe440c5d727838fa534f2b24027150602f87
Taylor Lea <taylorl@jfrog.com>
2021-02-09 09:29:25 -0800

    correct jq syntax

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -1,6 +1,6 @@
 deployApplication() {
   printenv
-  scp -i ~/.ssh/myVM "$res_myApp_resourcePath" jq -n "$res_myVM_targets[0]":/opt/
+  scp -i ~/.ssh/myVM "$res_myApp_resourcePath" $(jq -n $res_myVM_targets[0]):/opt/
 #  local success=true
 #  local url=$(find_step_configuration_value "healthCheckUrl")
 #  {
