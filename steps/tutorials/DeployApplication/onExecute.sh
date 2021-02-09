34bb7fefb6535b1d6835adb913f1b393e8dcb7c8
Taylor Lea <taylorl@jfrog.com>
2021-02-09 14:08:18 -0800

    do it

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -1,12 +1,15 @@
 deployApplication() {
 #  printenv
   # TODO: install rsync on imageBuilds
-#  apt-get install -y rsync
-
-#  local ip_addr=$(jq "$res_myVM_targets"[0] --raw-output --null-input)
+#   apt-get install -y rsync
+   local vm_cluster_name=$(get_resource_name --type VmCluster --operation IN)
+   local res_targets_var_name=res_"$vm_cluster_name"_targets
+   local ip_addr=$(jq $"$res_targets_var_name"[0] --raw-output --null-input)
+   echo "$ip_addr"
 #  local ssh_id="$HOME/.ssh/myVM"
-  get_resource_name --type VmCluster --operation IN
-  get_resource_name --type FileSpec --operation IN
+
+
+#  get_resource_name --type FileSpec --operation IN
 #  rsync -e "ssh -i $ssh_id" "$res_myApp_resourcePath" "$ip_addr":"$step_configuration_targetDirectory"
 #  ssh -i "$ssh_id" "$ip_addr" "cd $step_configuration_targetDirectory; $step_configuration_deployCommand"
 
