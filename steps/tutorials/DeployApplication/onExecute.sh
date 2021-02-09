f85dbf92ce36fe8f4bead101b3fc2509c5f5b227
Taylor Lea <taylorl@jfrog.com>
2021-02-09 12:39:41 -0800

    update

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -1,23 +1,12 @@
 deployApplication() {
   # TODO: install rsync on imageBuilds
   apt-get install -y rsync
-  printenv
 
-  ls "$res_myApp_resourcePath"
-  local ip_addr=$(jq $res_myVM_targets[0] --raw-output --null-input)
-  rsync -e "ssh -i ~/.ssh/myVM" "$res_myApp_resourcePath"/ "$ip_addr":/opt/
-#  ssh
-#  local success=true
-#  local url=$(find_step_configuration_value "healthCheckUrl")
-#  {
-#    local statusCode=$(curl --silent --output /dev/stderr --write-out "%{http_code}" "$url")
-#  } || exitCode=$?
-#  if test $statusCode -ne 200; then
-#    export success=false
-#    echo "Health check failed with statusCode: $statusCode & exitCode: $exitCode for url: $url"
-#  else
-#    echo "Health check succeeded"
-#  fi
+  local ip_addr=$(jq "$res_myVM_targets"[0] --raw-output --null-input)
+  local ssh_id="$HOME/.ssh/myVM"
+
+  rsync -e "ssh -i $ssh_id" "$res_myApp_resourcePath" "$ip_addr":"$step_configuration_targetDirectory"
+  ssh -i
 
   echo "deployApplication running"
 }
