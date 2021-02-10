230722ffd2f1e949e6331f180c66c4b506c4f310
Taylor Lea <taylorl@jfrog.com>
2021-02-10 14:49:45 -0800

    install rsync

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -5,16 +5,16 @@ deployApplication() {
   local app_filespec_name=$(get_resource_name --type FileSpec --operation IN)
   local res_targets=res_"$vm_cluster_name"_targets
   local app_resource_path=res_"$app_filespec_name"_resourcePath
-  local ip_addr=$(jq "${!res_targets}"[0] --raw-output --null-input)
   local ssh_id="$HOME/.ssh/$vm_cluster_name"
+  local ip_addr=$(jq "${!res_targets}"[0] --raw-output --null-input)
+  for i in "${!res_targets[@]}}"
+  do
+    :
+    echo "ipaddr >> $i"
+  done
 
-  echo "here"
-  echo $app_resource_path
-  echo "there"
-  ls $res_myApp_resourcePath
-  ls ${!app_resource_path}
-  rsync -e "ssh -i $ssh_id" "${!app_resource_path}" "$ip_addr":"$step_configuration_targetDirectory" --ignore-times
-  ssh -i "$ssh_id" "$ip_addr" "cd $step_configuration_targetDirectory; $step_configuration_deployCommand"
+  rsync -avH "${!app_resource_path}" -e "ssh -i $ssh_id" "$ip_addr":"$step_configuration_targetDirectory" --ignore-times
+  ssh -i "$ssh_id" "$ip_addr" "cd $step_configuration_targetDirectory/$app_filespec_name; $step_configuration_deployCommand"
 
   echo "deployApplication running"
 }
