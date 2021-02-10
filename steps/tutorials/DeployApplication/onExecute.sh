37439974daf19cd527c2f2466672dc37f367e0ae
Taylor Lea <taylorl@jfrog.com>
2021-02-10 14:39:35 -0800

    install rsync

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -1,5 +1,5 @@
 deployApplication() {
-#  apt-get install -y rsync
+  apt-get install -y rsync
 
   local vm_cluster_name=$(get_resource_name --type VmCluster --operation IN)
   local app_filespec_name=$(get_resource_name --type FileSpec --operation IN)
@@ -7,8 +7,6 @@ deployApplication() {
   local app_resource_path=res_"$app_filespec_name"_resourcePath
   local ip_addr=$(jq "${!res_targets}"[0] --raw-output --null-input)
   local ssh_id="$HOME/.ssh/$vm_cluster_name"
-
-  ls ${!app_resource_path}
 
   rsync -e "ssh -i $ssh_id" "${!app_resource_path}" "$ip_addr":"$step_configuration_targetDirectory" --ignore-times
   ssh -i "$ssh_id" "$ip_addr" "cd $step_configuration_targetDirectory; $step_configuration_deployCommand"
