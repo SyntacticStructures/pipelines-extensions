16e6dcd373fe641a583dd64f5d181e140099bb94
Taylor Lea <taylorl@jfrog.com>
2021-02-09 20:59:45 -0800

    ndebug

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -1,17 +1,18 @@
 deployApplication() {
 #  printenv
   # TODO: install rsync on imageBuilds
-#   apt-get install -y rsync
+   apt-get install -y rsync
+
    local vm_cluster_name=$(get_resource_name --type VmCluster --operation IN)
-   local res_targets_var_name=res_"$vm_cluster_name"_targets
-   echo $res_targets_var_name
-   local ip_addr=$(jq "${!res_targets_var_name}"[0] --raw-output --null-input)
-   echo "$ip_addr"
-#  local ssh_id="$HOME/.ssh/myVM"
+   local app_filespec_name=$(get_resource_name --type FileSpec --operation IN)
+   local res_targets=res_"$vm_cluster_name"_targets
+   local app_resource_path_var_name=res_"$app_filespec_name"_
+   local ip_addr=$(jq "${!res_targets}"[0] --raw-output --null-input)
+   local ssh_id="$HOME/.ssh/$vm_cluster_name"
 
 
 #  get_resource_name --type FileSpec --operation IN
-#  rsync -e "ssh -i $ssh_id" "$res_myApp_resourcePath" "$ip_addr":"$step_configuration_targetDirectory"
+  rsync -e "ssh -i $ssh_id" "$res_myApp_resourcePath" "$ip_addr":"$step_configuration_targetDirectory"
 #  ssh -i "$ssh_id" "$ip_addr" "cd $step_configuration_targetDirectory; $step_configuration_deployCommand"
 
   echo "deployApplication running"
