4dde5c1908f93b1d5367c68801cc1c5a3d60fbf3
Taylor Lea <taylorl@jfrog.com>
2021-02-10 17:44:49 -0800

    update

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -10,8 +10,14 @@ deployApplication() {
 
   # Iterate over json array of vm addresses.
   # We can't use a regular for loop because it's a json string, not a bash array.
-  echo "${!res_targets}" | jq -c '.[]' --raw-output | while read -r vm_addr; do
-    echo "Deploying $app_filespec_name to $vm_addr..."
+  echo "${!res_targets}" | jq -c '.[]' --raw-output | while ((i++)); read -r vm_addr; do
+
+    if [ -n "$step_configuration_rolloutDelay" ] && [ "$i" != 0 ] ; then
+      echo "Waiting ${step_configuration_rolloutDelay}s before next deploy"
+
+    fi
+
+    echo "Deploying $app_filespec_name to $vm_addr"
 
     # Upload app dir to vm, preserving any hardlinks or permissions
     rsync "${!app_resource_path}" -e "ssh -i $ssh_id" "$vm_addr":"$step_configuration_targetDirectory" \
@@ -21,7 +27,7 @@ deployApplication() {
     --perms
 
     # Run the deploy command from within the uploaded dir
-    echo "Running $step_configuration_deployCommand on $vm_addr..."
+    echo "Running $step_configuration_deployCommand on $vm_addr"
     ssh -i "$ssh_id" \
     -n "$vm_addr" \
     "cd $step_configuration_targetDirectory/$app_filespec_name; $step_configuration_deployCommand"
