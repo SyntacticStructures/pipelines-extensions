2c11e1349773f5eaf10cb70c6082ba74caad7262
Taylor Lea <taylorl@jfrog.com>
2021-02-16 15:04:07 -0800

    upload the tarball instead of the app

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -38,7 +38,7 @@ DeployApplication() {
     fi
 
     # Command to upload app dir to vm, preserving any hardlinks or permissions
-    local upload_command="rsync ${!app_resource_path} -e \"ssh -i $ssh_id\" $vm_addr:$step_configuration_targetDirectory \
+    local upload_command="rsync ./$app_filespec_tarball_name -e \"ssh -i $ssh_id\" $vm_addr:$step_configuration_targetDirectory \
     --ignore-times \
     --archive \
     --hard-links \
