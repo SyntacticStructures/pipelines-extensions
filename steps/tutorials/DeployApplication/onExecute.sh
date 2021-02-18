6a47ee3090cd44991970668b19cb545f44092fd3
Taylor Lea <taylorl@jfrog.com>
2021-02-17 16:47:08 -0800

    try to upload buildinfo to vm

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -9,67 +9,64 @@ DeployApplication() {
   local res_targets=res_"$vm_cluster_name"_targets
   local filespec_res_path=res_"$filespec_res_name"_resourcePath
   local ssh_id="$HOME/.ssh/$vm_cluster_name"
-
+  local vm_addrs=( $(echo "${!res_targets}" | jq --raw-output '.[]') )
 
   # We put everything we want to upload to vms in a directory
   # We will create a tarball from all of it
   local tardir="${PWD}/work"
   mkdir "$tardir"
-  cd "$tardir"
+  pushd "$tardir"
+    # download buildInfo artifacts to tardir
+    execute_command "jfrog rt dl "*" $tardir/ --build=${!buildinfo_name}/${!buildinfo_number}"
+    # move the fileSpec to tardir
+    mv "${!filespec_res_path}"/* "$tardir"/
+
+    # creat tarball from everything in the tardir
+    local tarball_name="$pipeline_name-$run_id.tar.gz"
+    execute_command "tar -czvf ../$tarball_name ."
+  popd
+
+  for i in "${!vm_addrs[@]}"
+  do
+
+    local vm_addr="${vm_addrs[$i]}"
+
+    # Wait between deploys if delay was specified
+    if [ -n "$step_configuration_rolloutDelay" ] && [ "$i" != 1 ]; then
+      execute_command "sleep ${step_configuration_rolloutDelay}s"
+    fi
+
+    # Command to upload app tarball to vm
+    local upload_command="scp -i $ssh_id ./$tarball_name $vm_addr:$step_configuration_targetDirectory"
+
+    # Command to run the deploy command from within the uploaded dir
+    local untar="cd $step_configuration_targetDirectory/; tar -xvf $tarball_name; rm -f $tarball_name;"
+    local deploy_command="ssh -i $ssh_id -n $vm_addr \"$untar $step_configuration_deployCommand\""
+
+    # Command to run after the deploy command from within the uploaded dir
+    local post_deploy_command="ssh -i $ssh_id \
+    -n $vm_addr \
+    \"cd $step_configuration_targetDirectory; $step_configuration_postDeployCommand\""
 
 
-  # download buildInfo artifacts to tardir
-  execute_command "jfrog rt dl "*" $tardir/ --build=${!buildinfo_name}/${!buildinfo_number}"
-  # move the fileSpec to tardir
-  mv "${!filespec_res_path}"/* "$tardir"/
+    # Don't exit on failed commands if fastFail is specified as false
+    if [ -n "$step_configuration_fastFail" ] && [ "$step_configuration_fastFail" == false ]; then
+      ignore_failure_suffix=" || continue"
+      upload_command+="$ignore_failure_suffix"
+      deploy_command+="$ignore_failure_suffix"
+      if [ -n "$post_deploy_command" ]; then
+        post_deploy_command+="$ignore_failure_suffix"
+      fi
+    fi
 
-  app_filespec_tarball_name="$filespec_res_name.tar.gz"
-  execute_command "tar -czvf $app_filespec_tarball_name ${!filespec_res_path}"
+    execute_command "$upload_command"
+    execute_command "$deploy_command"
 
-  ls "$tardir"
+    if [ -n "$post_deploy_command" ]; then
+      execute_command "$post_deploy_command"
+    fi
 
-#  for i in "${!vm_addrs[@]}"
-#  do
-#
-#    local vm_addr="${vm_addrs[$i]}"
-#
-#    # Wait between deploys if delay was specified
-#    if [ -n "$step_configuration_rolloutDelay" ] && [ "$i" != 1 ]; then
-#      execute_command "sleep ${step_configuration_rolloutDelay}s"
-#    fi
-#
-#    # Command to upload app tarball to vm
-#    local upload_command="scp -i $ssh_id ./$app_filespec_tarball_name $vm_addr:$step_configuration_targetDirectory"
-#
-#    # Command to run the deploy command from within the uploaded dir
-#    local untar="cd $step_configuration_targetDirectory/; tar -xvf $app_filespec_tarball_name; rm -f $app_filespec_tarball_name;"
-#    local deploy="cd $app_filespec_name; $step_configuration_deployCommand;"
-#    local deploy_command="ssh -i $ssh_id -n $vm_addr \"$untar $deploy\""
-#
-#    # Command to run after the deploy command from within the uploaded dir
-#    local post_deploy_command="ssh -i $ssh_id \
-#    -n $vm_addr \
-#    \"cd $step_configuration_targetDirectory/$app_filespec_name; $step_configuration_postDeployCommand\""
-#
-#
-#    # Don't exit on failed commands if fastFail is specified as false
-#    if [ -n "$step_configuration_fastFail" ] && [ "$step_configuration_fastFail" == false ]; then
-#      ignore_failure_suffix=" || continue"
-#      upload_command+="$ignore_failure_suffix"
-#      deploy_command+="$ignore_failure_suffix"
-#      if [ -n "$post_deploy_command" ]; then
-#        post_deploy_command+="$ignore_failure_suffix"
-#      fi
-#    fi
-#
-#    execute_command "$upload_command"
-#    execute_command "$deploy_command"
-#
-#    if [ -n "$post_deploy_command" ]; then
-#      execute_command "$post_deploy_command"
-#    fi
-#
-#  done
+  done
 }
 
 execute_command DeployApplication
