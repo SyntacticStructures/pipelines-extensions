5bd80a4cc99730db6cd985de049ab9d7621f2276
Taylor Lea <taylorl@jfrog.com>
2021-02-24 13:45:06 -0800

    try to get vmcluster to print windows

--- steps/tutorials/DeployApplication/onExecute.ps1
+++ steps/tutorials/DeployApplication/onExecute.ps1
@@ -1,6 +1,4 @@
 function DeployApplication() {
-  echo "hello deployApp"
-  gci env:* | sort-object name
   $buildinfo_res_name = $(get_resource_name -type BuildInfo -operation "IN")
   $buildinfo_number = "res_$($buildinfo_res_name)_buildNumber"
   $buildinfo_name = "res_$($buildinfo_res_name)_buildName"
