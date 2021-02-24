86e8731e53e719a36a1ee6f286409cc4181a725b
Taylor Lea <taylorl@jfrog.com>
2021-02-24 14:51:02 -0800

    try to get vmcluster to print windows

--- steps/tutorials/DeployApplication/onExecute.ps1
+++ steps/tutorials/DeployApplication/onExecute.ps1
@@ -1,14 +1,14 @@
 function DeployApplication() {
 #  gci env:* | sort-object name
   $buildinfo_res_name = $(get_resource_name -type BuildInfo -operation "IN")
-  $buildinfo_number = "res_$($buildinfo_res_name)_buildNumber"
-  $buildinfo_name = "res_$($buildinfo_res_name)_buildName"
-  $buildinfo_rt_url = "res_$($buildinfo_res_name)_sourceArtifactory_url"
-  $buildinfo_rt_user = "res_$($buildinfo_res_name)_sourceArtifactory_user"
-  $buildinfo_rt_apiKey = "res_$($buildinfo_res_name)_sourceArtifactory_apiKey"
+  $buildinfo_number = $((Get-Variable -Name "res_$($buildinfo_res_name)_buildNumber").Value)
+  $buildinfo_name = $((Get-Variable -Name "res_$($buildinfo_res_name)_buildName").Value)
+  $buildinfo_rt_url = $((Get-Variable -Name "res_$($buildinfo_res_name)_sourceArtifactory_url").Value)
+  $buildinfo_rt_user = $((Get-Variable -Name "res_$($buildinfo_res_name)_sourceArtifactory_user").Value)
+  $buildinfo_rt_apiKey = $((Get-Variable -Name "res_$($buildinfo_res_name)_sourceArtifactory_apikey").Value)
 
   $filespec_res_name = $(get_resource_name -type FileSpec -operation "IN")
-  $filespec_res_path = "res_$($filespec_res_name)_resourcePath"
+  $filespec_res_path = $((Get-Variable -Name "res_$($filespec_res_name)_resourcePath").Value)
 
   $releasebundle_res_name = $(get_resource_name -type ReleaseBundle -operation "IN")
 
@@ -25,11 +25,14 @@ function DeployApplication() {
   mkdir $tardir
 
   if ($buildinfo_res_name -ne "") {
-    echo "vars..."
     echo $buildinfo_rt_url
     echo $buildinfo_rt_user
     echo $buildinfo_rt_apiKey
-    execute_command "jfrog rt dl \"*\" $tardir/ --build=$buildinfo_name/buildinfo_number --url=$buildinfo_rt_url --user=$buildinfo_rt_user --apikey=$buildinfo_rt_apikey"
+    execute_command "jfrog rt dl \"*\" $tardir/ \
+    --build=$buildinfo_name/buildinfo_number \
+    --url=$buildinfo_rt_url \
+    --user=$buildinfo_rt_user \
+    --apikey=$buildinfo_rt_apikey"
 
     ls $tardir
   } elseif ($filespec_res_name -ne "") {
