203e52011fe9d70c7924490c4f57bafc5252dbf1
Taylor Lea <taylorl@jfrog.com>
2021-02-24 13:32:00 -0800

    try to get vmcluster to print windows

--- steps/tutorials/DeployApplication/onExecute.ps1
+++ steps/tutorials/DeployApplication/onExecute.ps1
@@ -1,10 +1,25 @@
 function DeployApplication() {
 #  gci env:* | sort-object name
   $buildinfo_res_name = $(get_resource_name -type BuildInfo -operation "IN")
-  $vm_cluster_name = $(get_resource_name -type VmCluster -operation "IN")
+  $buildinfo_number = "res_$($buildinfo_res_name)_buildNumber"
+  $buildinfo_name = "res_$($buildinfo_res_name)_buildName"
 
-  $vm_targets=$((Get-Variable -Name "res_$($vm_cluster_name)_targets").Value | ConvertFrom-Json)
-  echo $vm_targets[0]
+  $filespec_res_name = $(get_resource_name -type FileSpec -operation "IN")
+  $filespec_res_path = "res_$($filespec_res_name)_resourcePath"
+
+  $releasebundle_res_name = $(get_resource_name -type ReleaseBundle -operation "IN")
+
+  $vmcluster_res_name = $(get_resource_name -type VmCluster -operation "IN")
+  $vm_targets = $((Get-Variable -Name "res_$($vmcluster_res_name)_targets").Value | ConvertFrom-Json)
+
+  if (Test-Path $buildinfo_res_name) {
+    echo "res name"
+    echo $buildinfo_res_name
+  }
+
+  if (Test-Path $filespec_res_name) {
+    echo "well crap"
+  }
 }
 
 execute_command DeployApplication
