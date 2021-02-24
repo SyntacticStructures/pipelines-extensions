aa71bff12c7b3fd9fab98e72c904da6e810c0b89
Taylor Lea <taylorl@jfrog.com>
2021-02-24 12:39:01 -0800

    add buildinfo resource

--- steps/tutorials/DeployApplication/onExecute.ps1
+++ steps/tutorials/DeployApplication/onExecute.ps1
@@ -1,9 +1,10 @@
 function DeployApplication() {
-  gci env:* | sort-object name
+#  gci env:* | sort-object name
   $buildinfo_res_name = $(get_resource_name -type BuildInfo -operation "IN")
   $vm_cluster_name = $(get_resource_name -type VmCluster -operation "IN")
 
   echo $buildinfo_res_name
+
   echo $vm_cluster_name
 }
 
