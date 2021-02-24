183cfe17091c4e67485c112b2507f54c5145bb89
Taylor Lea <taylorl@jfrog.com>
2021-02-24 13:22:31 -0800

    try to get vmcluster to print windows

--- steps/tutorials/DeployApplication/onExecute.ps1
+++ steps/tutorials/DeployApplication/onExecute.ps1
@@ -3,11 +3,8 @@ function DeployApplication() {
   $buildinfo_res_name = $(get_resource_name -type BuildInfo -operation "IN")
   $vm_cluster_name = $(get_resource_name -type VmCluster -operation "IN")
 
-  echo $buildinfo_res_name
-
-  echo $vm_cluster_name
-  $p=$((Get-Variable -Name "res_$($vm_cluster_name)_targets").Value)
-  echo $p
+  $vm_targets=$((Get-Variable -Name "res_$($vm_cluster_name)_targets").Value | ConvertFrom-Json)
+  echo $vm_targets[0]
 }
 
 execute_command DeployApplication
