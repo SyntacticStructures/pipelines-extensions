e9399e586a005b17bd313d7044a5c7b7fb4093bc
Taylor Lea <taylorl@jfrog.com>
2021-02-24 14:40:05 -0800

    try to get vmcluster to print windows

--- steps/tutorials/DeployApplication/onExecute.ps1
+++ steps/tutorials/DeployApplication/onExecute.ps1
@@ -3,6 +3,9 @@ function DeployApplication() {
   $buildinfo_res_name = $(get_resource_name -type BuildInfo -operation "IN")
   $buildinfo_number = "res_$($buildinfo_res_name)_buildNumber"
   $buildinfo_name = "res_$($buildinfo_res_name)_buildName"
+  $buildinfo_rt_url = "res_$($buildinfo_res_name)_sourceArtifactory_url"
+  $buildinfo_rt_user = "res_$($buildinfo_res_name)_sourceArtifactory_user"
+  $buildinfo_rt_apiKey = "res_$($buildinfo_res_name)_sourceArtifactory_apiKey"
 
   $filespec_res_name = $(get_resource_name -type FileSpec -operation "IN")
   $filespec_res_path = "res_$($filespec_res_name)_resourcePath"
@@ -23,7 +26,12 @@ function DeployApplication() {
 
   if ($buildinfo_res_name -ne "") {
     execute_command "jfrog rt config show"
-    execute_command "jfrog rt dl \"*\" $tardir/ --build=$buildinfo_name/buildinfo_number"
+    execute_command "jfrog rt dl \"*\" $tardir/ \
+    --build=$buildinfo_name/buildinfo_number \
+    --url=$buildinfo_rt_url \
+    --user=$buildinfo_rt_user \
+    --apikey=$buildinfo_rt_apikey"
+
     ls $tardir
   } elseif ($filespec_res_name -ne "") {
 
