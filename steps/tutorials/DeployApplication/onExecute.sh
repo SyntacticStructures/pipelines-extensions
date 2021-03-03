06fc05504c3c6e5f2ad7f79a6b6a67071db753a7
Taylor Lea <taylorl@jfrog.com>
2021-03-03 14:29:08 -0800

    fix curl console output

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -92,7 +92,8 @@ DeployApplication() {
       local distribution_user=res_"$release_bundle_res_name"_sourceDistribution_user
       # TODO: Check for ready status before exporting. It may already by exported.
       local resp_body_file="$step_tmp_dir/response.json"
-      getDistributionExportStatus "${!distribution_url}" "${!release_bundle_name}" "${!release_bundle_version}" "${!distribution_user}" "${!distribution_apikey}" "$resp_body_file"
+      local status=$(getDistributionExportStatus "${!distribution_url}" "${!release_bundle_name}" "${!release_bundle_version}" "${!distribution_user}" "${!distribution_apikey}" "$resp_body_file")
+      execute_command "echo $status"
     fi
     # create tarball from everything in the tardir
     local tarball_name="$pipeline_name-$run_id.tar.gz"
