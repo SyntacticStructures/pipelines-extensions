be488c51e2b876d4b228b553c1b27105bd6ce7a1
Taylor Lea <taylorl@jfrog.com>
2021-03-03 09:41:12 -0800

    try to force update

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -1,3 +1,47 @@
+#  local release_bundle_res_name=$(get_resource_name --type ReleaseBundle --operation IN)
+#  local release_bundle_version=res_"$release_bundle_res_name"_version
+#  local release_bundle_name=res_"$release_bundle_res_name"_name
+#  local distribution_url=res_"$release_bundle_res_name"_sourceDistribution_url
+#  local distribution_user=res_"$release_bundle_res_name"_sourceDistribution_user
+#  local distribution_apikey=res_"$release_bundle_res_name"_sourceDistribution_apikey
+#  local distribution_user=res_"$release_bundle_res_name"_sourceDistribution_user
+#
+#  # We call this endpoint to make sure the release bundle is ready.
+#  # If it's ready it will return a download url.
+#  local release_bundle_status_url="${!distribution_url}/api/v1/export/release_bundle/${!release_bundle_name}/${!release_bundle_version}/status"
+#  local bundle_download_url=$(curl -XGET "$release_bundle_status_url" -u "${!distribution_user}:${!distribution_apikey}" | jq ."download_url")
+#
+#
+#  local buildinfo_res_name=$(get_resource_name --type BuildInfo --operation IN)
+#  local buildinfo_number=res_"$buildinfo_res_name"_buildNumber
+#  local buildinfo_name=res_"$buildinfo_res_name"_buildName
+#  local vm_cluster_name=$(get_resource_name --type VmCluster --operation IN)
+#  local filespec_res_name=$(get_resource_name --type FileSpec --operation IN)
+#  local res_targets=res_"$vm_cluster_name"_targets
+#  local filespec_res_path=res_"$filespec_res_name"_resourcePath
+#  local ssh_id="$HOME/.ssh/$vm_cluster_name"
+#  local vm_addrs=( $(echo "${!res_targets}" | jq --raw-output '.[]') )
+#
+#  # We put everything we want to upload to vms in a directory
+#  # We will create a tarball from all of it
+#  local tardir="${PWD}/work"
+#  mkdir "$tardir"
+#  pushd "$tardir"
+#    # download buildInfo artifacts to tardir
+#    execute_command "jfrog rt dl "*" $tardir/ --build=${!buildinfo_name}/${!buildinfo_number}"
+#    # move the fileSpec to tardir
+#    mv "${!filespec_res_path}"/* "$tardir"/
+#
+#    # download and unzip release bundle
+#    curl --remote-name -XGET "$bundle_download_url" -u "${!distribution_user}:${!distribution_apikey}"
+#    unzip "${!release_bundle_name}"-"${!release_bundle_version}".zip
+#
+#    # creat tarball from everything in the tardir
+#    local tarball_name="$pipeline_name-$run_id.tar.gz"
+#    execute_command "tar -czvf ../$tarball_name ."
+#  popd
+
+
 DeployApplication() {
   local buildinfo_res_name=$(get_resource_name --type BuildInfo --operation IN)
 
