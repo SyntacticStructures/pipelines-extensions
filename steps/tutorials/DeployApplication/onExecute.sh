f3dabfc825b92e0ae8cd71314202650e702f1d72
Taylor Lea <taylorl@jfrog.com>
2021-03-04 12:06:10 -0800

    try benhas release bundle logic

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -223,7 +223,7 @@ getDistributionExportStatus() {
   execute_command "echo : distribution_user      $distribution_user"
   execute_command "echo : distribution_apikey    $distribution_apikey"
 
-  local request="curl $curl_options $distribution_url/api/v1/export/release_bundle/${!release_bundle_name}/${!release_bundle_version}/status"
+  local request="curl $curl_options $distribution_url/api/v1/export/release_bundle/$release_bundle_name/$release_bundle_version/status"
   execute_command "echo $request"
   $request
 }
