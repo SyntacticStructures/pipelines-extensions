92c236c1d115f214087eede0982bdc690a080c61
Taylor Lea <taylorl@jfrog.com>
2021-03-04 12:14:47 -0800

    remove outer execute_command

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -209,7 +209,7 @@ getDistributionExportStatus() {
   fi
 
   local request="curl $curl_options $distribution_url/api/v1/export/release_bundle/$release_bundle_name/$release_bundle_version/status"
-  execute_command "$request"
+  $request
 }
 
 exportReleaseBundle() {
