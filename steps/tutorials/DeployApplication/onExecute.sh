bffb1a71ccfa27f60b13dae625d8a90a7b053cf5
Taylor Lea <taylorl@jfrog.com>
2021-03-04 15:54:58 -0800

    big changes to release bundle here

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -40,20 +40,28 @@
 #    local tarball_name="$pipeline_name-$run_id.tar.gz"
 #    execute_command "tar -czvf ../$tarball_name ."
 #  popd
+export resp_body_file="$step_tmp_dir/response.json"
+export release_bundle_version
+export release_bundle_name
+export distribution_url
+export distribution_user
+export distribution_apikey
+export distribution_request_args
+export distribution_curl_options
 
 
 DeployApplication() {
   local buildinfo_res_name=$(get_resource_name --type BuildInfo --operation IN)
 
   local filespec_res_name=$(get_resource_name --type FileSpec --operation IN)
-  local filespec_res_path=res_"$filespec_res_name"_resourcePath
+  local filespec_res_path=$(eval echo "$"res_"$filespec_res_name"_resourcePath)
 
   local releasebundle_res_name=$(get_resource_name --type ReleaseBundle --operation IN)
 
   local vm_cluster_name=$(get_resource_name --type VmCluster --operation IN)
-  local res_targets=res_"$vm_cluster_name"_targets
+  local res_targets=$(eval echo "$"res_"$vm_cluster_name"_targets)
   local ssh_id="$HOME/.ssh/$vm_cluster_name"
-  local vm_addrs=( $(echo "${!res_targets}" | jq --raw-output '.[]') )
+  local vm_addrs=( $(echo "$res_targets" | jq --raw-output '.[]') )
 
   res_types=( $buildinfo_res_name $filespec_res_name $releasebundle_res_name )
   if [ "${#res_types[@]}" != 1 ]; then
@@ -70,85 +78,96 @@ DeployApplication() {
   pushd "$tardir"
     if [ -n "$buildinfo_res_name" ]; then
       # download buildInfo artifacts to tardir
-      local buildinfo_number=res_"$buildinfo_res_name"_buildNumber
-      local buildinfo_name=res_"$buildinfo_res_name"_buildName
+      local buildinfo_number=$(eval echo "$"res_"$buildinfo_res_name"_buildNumber)
+      local buildinfo_name=$(eval echo "$"res_"$buildinfo_res_name"_buildName)
       local integration_alias
       integration_alias=$(find_resource_variable "$buildinfo_res_name" integrationAlias)
-      local rt_url=res_"$buildinfo_res_name"_"$integration_alias"_url
-      local rt_user=res_"$buildinfo_res_name"_"$integration_alias"_user
-      local rt_apikey=res_"$buildinfo_res_name"_"$integration_alias"_apikey
-      execute_command "retry_command jfrog rt config --insecure-tls=$no_verify_ssl --url ${!rt_url} --user ${!rt_user} --apikey ${!rt_apikey} --interactive=false"
-      execute_command "jfrog rt dl \"*\" $tardir/ --build=${!buildinfo_name}/${!buildinfo_number}"
+      local rt_url=$(eval echo "$"res_"$buildinfo_res_name"_"$integration_alias"_url)
+      local rt_user=$(eval echo "$"res_"$buildinfo_res_name"_"$integration_alias"_user)
+      local rt_apikey=$(eval echo "$"res_"$buildinfo_res_name"_"$integration_alias"_apikey)
+      execute_command "retry_command jfrog rt config --insecure-tls=$no_verify_ssl --url $rt_url --user $rt_user --apikey $rt_apikey --interactive=false"
+      execute_command "jfrog rt dl \"*\" $tardir/ --build=$buildinfo_name/$buildinfo_number"
     elif [ -n "$filespec_res_name" ]; then
       # move the fileSpecs to tardir
-      execute_command "mv ${!filespec_res_path}/* $tardir/"
+      execute_command "mv $filespec_res_path/* $tardir/"
     elif [ "$releasebundle_res_name" ]; then
       # Export and download release bundle
-      local release_bundle_res_name
-      release_bundle_res_name=$(get_resource_name --type ReleaseBundle --operation IN)
-      local release_bundle_version=res_"$release_bundle_res_name"_version
-      local release_bundle_name=res_"$release_bundle_res_name"_name
-      local distribution_url=res_"$release_bundle_res_name"_sourceDistribution_url
-      local distribution_user=res_"$release_bundle_res_name"_sourceDistribution_user
-      local distribution_apikey=res_"$release_bundle_res_name"_sourceDistribution_apikey
-      local resp_body_file="$step_tmp_dir/response.json"
-      local distribution_request_args=("${!distribution_url}" "${!release_bundle_name}" "${!release_bundle_version}" "${!distribution_user}" "${!distribution_apikey}" "$resp_body_file")
+      local release_bundle_res_name=$(get_resource_name --type ReleaseBundle --operation IN)
+      release_bundle_version=$(eval echo "$"res_"$release_bundle_res_name"_version)
+      release_bundle_name=$(eval echo "$"res_"$release_bundle_res_name"_name)
+      distribution_url=$(eval echo "$"res_"$release_bundle_res_name"_sourceDistribution_url)
+      distribution_user=$(eval echo "$"res_"$release_bundle_res_name"_sourceDistribution_user)
+      distribution_apikey=$(eval echo "$"res_"$release_bundle_res_name"_sourceDistribution_apikey)
+      distribution_curl_options="-XGET --silent --retry 3 --write-out %{http_code}\n --output $resp_body_file -u $distribution_user:$distribution_apikey"
+      if [ "$no_verify_ssl" == "true" ]; then
+        distribution_curl_options+=" --insecure"
+      fi
+
       # Check if release bundle was already exported
-      local status_http_code
-      status_http_code=$(getDistributionExportStatus "${distribution_request_args[@]}")
-      execute_command "cat $resp_body_file"
-      execute_command "echo $status_http_code"
+      local status_http_code=$(getDistributionExportStatus)
 
-      # check status
-      if [ "$status_http_code" -eq 404 ]; then
-        execute_command "echo 'Release Bundle ${!release_bundle_name}/${!release_bundle_version} not found. Please check your Release Bundle details'"
-        execute_command "exit 1"
-      elif [ "$status_http_code" -eq 200 ]; then
-        execute_command "echo 'got here'"
-        local export_status
-        export_status=$(cat "$resp_body_file" | jq -r .status)
-        if [ "$export_status" == "NOT_TRIGGERED" ]; then
-          execute_command "echo 'Release Bundle ${!release_bundle_name}/${!release_bundle_version} export was not triggered yet. We will trigger it now'"
-        elif [ "$export_status" == "COMPLETED" ]; then
-          execute_command "echo 'Release Bundle ${!release_bundle_name}/${!release_bundle_version} export was already created. We will download it now'"
-          deleteExportedBundleOnFinish="false"
-          export_status="COMPLETED"
-        elif [ "$export_status" == "IN_PROGRESS" ] || [ "$export_status" == "NOT_EXPORTED" ] ; then
-          execute_command "echo 'Release Bundle ${!release_bundle_name}/${!release_bundle_version} export is in progress. Will wait for it to be completed'"
-          deleteExportedBundleOnFinish="false"
-          export_status="IN_PROGRESS"
-        elif [ "$export_status" == "FAILED" ]; then
-          execute_command "echo 'Release Bundle ${!release_bundle_name}/${!release_bundle_version} export failed before. Trying to trigger an export again.'"
-          export_status="FAILED"
-        fi
-      else
-        execute_command "echo 'Release Bundle ${!release_bundle_name}/${!release_bundle_version} status=$export_status is unexpected. Aborting... '"
+      # exit on bad response codes
+      if [ "$status_http_code" -ne 200 ]; then
+        execute_command "echo 'Could not get Release Bundle export status'"
+        execute_command "echo http status: $status_http_code"
         execute_command "exit 1"
       fi
+
+      # possible values: IN_PROGRESS|FAILED|NOT_TRIGGERED|NOT_EXPORTED|COMPLETED
+      local export_status=$(cat "$resp_body_file" | jq -r .status)
+
       # export the Release Bundle if hasn't been exported yet
-      if [ "$export_status" == "NOT_TRIGGERED" ] ||[ "$export_status" == "FAILED" ]; then
+      handleExportStatus "$export_status"
 
-        execute_command "echo 'Triggering Release Bundle download for ${!release_bundle_name}/${!release_bundle_version}'"
-        local export_http_code
-        export_http_code=$(exportReleaseBundle "${distribution_request_args[@]}")
-        if [ "$export_http_code" -gt 299 ]; then
-          execute_command "echo 'Triggering Release Bundle download failed ${!release_bundle_name}/${!release_bundle_version} failed with status code $export_http_code'"
-          if [ "$export_http_code" -eq 404 ]; then
-            execute_command "echo 'Release Bundle ${!release_bundle_name}/${!release_bundle_version} not found'"
+      # wait for Release Bundle to be exported and check its status
+      # we include COMPLETED job status here because we need the bundle archive download URL
+      if [ "$export_status" != "FAILED" ]; then
+        status_http_code=$(getDistributionExportStatus)
+        local sleeperCount=2
+        while [ "$status_http_code" -lt 299 ] && isReleaseBundleExporting; do
+          execute_command "sleep $sleeperCount"
+          sleeperCount+="$sleeperCount"
+          if [ $sleeperCount -gt 64 ]; then
+            # Keep checking for 128 seconds if export status hasn't reached COMPLETED yet
+            break
           fi
-          execute_command "exit 1"
-        elif [ "$export_http_code" -eq 202 ]; then
-          execute_command "echo 'Successfully scheduled export of Release Bundle ${!release_bundle_name}/${!release_bundle_version}'"
-        else
-          execute_command "echo 'Exporting Release Bundle ${!release_bundle_name}/${!release_bundle_version} finished with unexpected status code $export_http_code'"
+          status_http_code=$(getDistributionExportStatus)
+        done
+
+        # exit on bad response codes
+        if [ "$status_http_code" -ne 200 ]; then
+          execute_command "echo 'Could not get Release Bundle export status'"
+          execute_command "echo http status: $status_http_code"
           execute_command "exit 1"
         fi
-        export_status="TRIGGERED"
+
+        local resp_body=$(cat "$resp_body_file")
+        export_status=$(echo "$resp_body" | jq -r .status)
+
+        # exit on bad export status
+        if [ "$export_status" != "COMPLETED" ]; then
+          execute_command "echo 'Failed to export release bundle with status: $export_status'"
+        fi
+
+        execute_command "echo 'Exported Release Bundle $release_bundle_name/$release_bundle_version successfully'"
+
+        # download release bundle
+        local download_url="$(echo resp_body | jq -r .download_url)"
+        status_http_code=$(downloadReleaseBundle "$download_url")
+
+        # exit on bad response codes
+        if [ "$status_http_code" -ne 200 ]; then
+          execute_command "echo 'Could not get Release Bundle export status'"
+          execute_command "echo http status: $status_http_code"
+          execute_command "exit 1"
+        fi
+        execute_command "echo 'Downloaded Release Bundle $release_bundle_name/$release_bundle_version successfully'"
+
+        execute_command "unzip $resp_body"
       fi
+
+
     fi
-    execute_command "echo $export_status"
-    execute_command "echo killing the program"
-    execute_command "exit 1"
     # create tarball from everything in the tardir
     local tarball_name="$pipeline_name-$run_id.tar.gz"
     execute_command "tar -czvf ../$tarball_name ."
@@ -198,45 +217,46 @@ DeployApplication() {
 }
 
 getDistributionExportStatus() {
-  local distribution_url=$1
-  local release_bundle_name=$2
-  local release_bundle_version=$3
-  local distribution_user=$4
-  local distribution_apikey=$5
-  local resp_body_file=$6
-
-  local curl_options="-XGET --silent --retry 3 --write-out %{http_code}\n --output $resp_body_file -u $distribution_user:$distribution_apikey"
-
-  if [ "$no_verify_ssl" == "true" ]; then
-    curl_options+=" --insecure"
-  fi
-
-  local request="curl $curl_options $distribution_url/api/v1/export/release_bundle/$release_bundle_name/$release_bundle_version/status"
+  local request="curl $distribution_curl_options $distribution_url/api/v1/export/release_bundle/$release_bundle_name/$release_bundle_version/status"
   $request
 }
 
 exportReleaseBundle() {
-  local distribution_url=$1
-  local release_bundle_name=$2
-  local release_bundle_version=$3
-  local distribution_user=$4
-  local distribution_apikey=$5
-  local resp_body_file=$6
-  local curl_options="-XGET --silent --retry 3 --write-out '%{http_code}\n' --output $resp_body_file -u $distribution_user:$distribution_apikey"
-
-  execute_command "echo : distribution_url       $distribution_url"
-  execute_command "echo : release_bundle_version $release_bundle_version"
-  execute_command "echo : release_bundle_name    $release_bundle_name"
-  execute_command "echo : distribution_url       $distribution_url"
-  execute_command "echo : distribution_user      $distribution_user"
-  execute_command "echo : distribution_apikey    $distribution_apikey"
-
-  if [ "$no_verify_ssl" == "true" ]; then
-    curlOptions+=" --insecure"
-  fi
-  $export_status
-  local request="curl $curlOptions $distributionUrl/api/v1/export/release_bundle/$releaseBundleName/$releaseBundleVersion"
+  local request="curl $distribution_curl_options $distribution_url/api/v1/export/release_bundle/$release_bundle_name/$release_bundle_version"
   $request
+}
+
+downloadReleaseBundle() {
+  download_url=$1
+  local curl_options=$distribution_curl_options
+  curl_options+=
+  local request="curl $distribution_curl_options $download_url"
+}
+
+handleExportStatus() {
+  export_status=$1
+
+  # Trigger release bundle export if possible
+  if [ "$export_status" == "NOT_TRIGGERED" ] || [ "$export_status" == "FAILED" ]; then
+    execute_command "echo 'Exporting Release Bundle: $release_bundle_name/$release_bundle_version'"
+    local export_http_code
+    export_http_code=$(exportReleaseBundle)
+    if [ "$export_http_code" -ne 202 ]; then
+      execute_command "Failed to export release bundle"
+      execute_command "exit 1"
+    fi
+    execute_command "echo 'Started export of Release Bundle $release_bundle_name}/$release_bundle_version}'"
+  fi
+}
+
+isReleaseBundleExporting() {
+  local status
+  status=$(cat $resp_body_file | jq -r .status)
+  if [ "$status" == "IN_PROGRESS" ] ||
+   [ "$status" == "NOT_EXPORTED" ]; then
+    return
+  fi
+  false
 }
 
 execute_command DeployApplication
