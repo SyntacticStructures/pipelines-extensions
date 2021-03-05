3c87b9f3b91abbd7abf5ca3ffee73595279be004
Taylor Lea <taylorl@jfrog.com>
2021-03-05 12:55:45 -0800

    try deploy target overrides

--- steps/tutorials/DeployApplication/helpers.sh
+++ steps/tutorials/DeployApplication/helpers.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e -o pipefail
 
-export resp_body_file="$step_tmp_dir/response_body"
-export should_cleanup_export=false
-
 __getDistributionExportStatus() {
   local curl_options=$distribution_curl_options
   curl_options+=" -XGET"
@@ -64,8 +61,6 @@ downloadBuildInfo() {
 
 downloadReleaseBundle() {
   # Export and download release bundle
-  execute_command "echo 'printenv1'"
-  execute_command "printenv"
   local release_bundle_res_name=$1
   export release_bundle_version=$(eval echo "$"res_"$release_bundle_res_name"_version)
   export release_bundle_name=$(eval echo "$"res_"$release_bundle_res_name"_name)
@@ -73,13 +68,9 @@ downloadReleaseBundle() {
   export distribution_user=$(eval echo "$"res_"$release_bundle_res_name"_sourceDistribution_user)
   export distribution_apikey=$(eval echo "$"res_"$release_bundle_res_name"_sourceDistribution_apikey)
   export distribution_curl_options="--silent --retry 3 --write-out %{http_code}\n --output $resp_body_file -u $distribution_user:$distribution_apikey"
-  execute_command "echo '$release_bundle_version'"
-  execute_command "echo '$release_bundle_name'"
-  execute_command "echo '$distribution_url'"
-  execute_command "echo '$distribution_user'"
-  execute_command "echo '$distribution_apikey'"
-  execute_command "echo $res_myReleaseBundle2_version"
-  execute_command "echo $release_bundle_res_name"
+  export resp_body_file="$step_tmp_dir/response_body"
+  export should_cleanup_export=false
+
   if [ "$no_verify_ssl" == "true" ]; then
     distribution_curl_options+=" --insecure"
   fi
