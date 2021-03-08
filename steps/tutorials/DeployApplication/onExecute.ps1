cb4a04592bd7cf7db55ee967129fdc72323f5faa
Taylor Lea <taylorl@jfrog.com>
2021-03-08 13:58:04 -0800

    try importing helper script windows

--- steps/tutorials/DeployApplication/onExecute.ps1
+++ steps/tutorials/DeployApplication/onExecute.ps1
@@ -1,13 +1,7 @@
 $ErrorActionPreference = "Stop"
 
 # TODO: implement $DEPLOY_TARGETS_OVERRIDE
-
-function SetupSSH($key_name) {
-  $ssh_key_path = Join-Path $env:USERPROFILE -ChildPath ".ssh" | Join-Path -ChildPath $key_name
-  execute_command "Get-Service -Name ssh-agent | Set-Service -StartupType Manual"
-  execute_command "Start-Service ssh-agent"
-  execute_command "ssh-add $ssh_key_path"
-}
+. .\helpers.ps1
 
 function DeployApplication() {
   #  gci env:* | sort-object name
