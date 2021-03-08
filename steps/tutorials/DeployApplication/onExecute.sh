1ffd3b41a5096a4abcaae321796162ad5517ba14
Taylor Lea <taylorl@jfrog.com>
2021-03-08 08:44:28 -0800

    try to do env vars

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -7,6 +7,7 @@ DeployApplication() {
   local vm_env_file="$step_tmp_dir/vmEnv"
   if [ -n "$step_configuration_vmEnvironmentVariables_len" ];then
     execute_command "echo \"${step_configuration_vmEnvironmentVariables[0]}\""
+    execute_command "echo \"${#step_configuration_vmEnvironmentVariables[@]}\""
     for i in "${!step_configuration_vmEnvironmentVariables[@]}"; do
       execute_command "echo 'export ${step_configuration_vmEnvironmentVariables[$i]}' >> $vm_env_file"
       execute_command "echo 'export ${step_configuration_vmEnvironmentVariables[$i]}' >> $vm_env_file"
