1c6327d16a8c4644efea17ae9caef12d1e116a49
Taylor Lea <taylorl@jfrog.com>
2021-03-09 11:26:47 -0800

    debug release bundle ps1

--- steps/tutorials/DeployApplication/helpers.ps1
+++ steps/tutorials/DeployApplication/helpers.ps1
@@ -85,8 +85,8 @@ class ReleaseBundleDownloader {
   _getDistributionExportStatus() {
     execute_command "echo '_getDistributionExportStatus'"
 
-    $headers = $headers['Authorization'] = "Basic $encoded_auth"
-
+    $headers = @{}
+    $headers['Authorization'] = "Basic $($this.EncodedAuth)"
     execute_command "retry_command Invoke-WebRequest `"$($this.Url)/api/v1/export/release_bundle/$($this.BundleName)/$($this.BundleVersion)/status`" -Method Get -Headers `$authHeaders $($this.CommonRequestParams)"
     $exportStatus = (ConvertFrom-JSON (Get-Content "$($this.ResponseBodyFilePath)")).status
     return $exportStatus
