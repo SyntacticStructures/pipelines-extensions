13947d9f90e66e517738728c3a7acb2474b4d23f
Taylor Lea <taylorl@jfrog.com>
2021-03-09 11:31:17 -0800

    debug release bundle ps1

--- steps/tutorials/DeployApplication/onExecute.ps1
+++ steps/tutorials/DeployApplication/onExecute.ps1
@@ -61,7 +61,7 @@ function DeployApplication() {
     execute_command "echo 'we are here'"
     $releaseBundleDownloader = [ReleaseBundleDownloader]::new($releasebundle_res_name)
     execute_command "echo 'we are there'"
-    execute_command "Get-Member $releaseBundleDownloader"
+    execute_command "Get-Member -InputObject $releaseBundleDownloader"
     execute_cmmand "$($releaseBundleDownloader.Download())"
   }
   $tarballName = "${pipeline_name-$run_id}.tar.gz"
