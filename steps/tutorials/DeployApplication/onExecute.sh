1165bdde220533aa0402fefee01efbc7ed6317ac
Taylor Lea <taylorl@jfrog.com>
2021-03-11 09:15:21 -0800

    trying to fix bash ssh

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -16,6 +16,11 @@ DeployApplication() {
   local ssh_id="$HOME/.ssh/$vm_cluster_name"
   local vm_targets=( $(echo "$res_targets" | jq --raw-output '.[]') )
 
+  local ssh_base_command="ssh -i $ssh_id -n $vm_target"
+
+  execute_command "$ssh_base_command ls /"
+  exit 1
+
   res_types=( $buildinfo_res_name $filespec_res_name $releasebundle_res_name )
   if [ "${#res_types[@]}" != 1 ]; then
     execute_command "echo Exactly one resource of type BuildInfo\|ReleaseBundle\|FileSpec is supported."
