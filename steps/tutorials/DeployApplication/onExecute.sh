ef7e8e981365da90c9c0dcac692e0112f87bd51c
Taylor Lea <taylorl@jfrog.com>
2021-03-15 09:58:26 -0700

    trying ssh-add in bash

--- steps/tutorials/DeployApplication/onExecute.sh
+++ steps/tutorials/DeployApplication/onExecute.sh
@@ -17,6 +17,7 @@ DeployApplication() {
 #  local vm_targets=( $(echo "$res_targets" | jq --raw-output '.[]') )
 #
   execute_command "cat $ssh_id"
+  execute_command "cat $vm_cluster_name"
   execute_command "ssh -i /root/.ssh/myVM -n root@192.168.50.3 -o StrictHostKeyChecking=no \"ls /\""
 #
 #  res_types=( $buildinfo_res_name $filespec_res_name $releasebundle_res_name )
